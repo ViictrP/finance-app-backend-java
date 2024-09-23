a5c59b96c3b01c54b786fda828f421d872c6d576
Victor Prado <victor.prado@arc-one.com>
2024-09-23 00:37:22 -0300

    Upgrade Java: removed

--- src/main/java/com/victorprado/financeappbackendjava/domain/exception/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.victorprado.financeappbackendjava.domain.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class UserNotFoundException extends CoreException {
-    public UserNotFoundException() {
-        super(HttpStatus.NOT_FOUND, "User not found!");
-    }
-}
--- src/main/java/com/victorprado/financeappbackendjava/service/dto/ProfileCriteria.java
+++
@@ -1,9 +0,0 @@
-package com.victorprado.financeappbackendjava.service.dto;
-
-import lombok.Data;
-
-@Data
-public class ProfileCriteria {
-  private String month;
-  private Integer year;
-}
